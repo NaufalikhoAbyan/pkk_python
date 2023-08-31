d02e0e313b1207df15f90bf0ca95b357ff5b2715
NaufalikhoAbyan <apradana448@gmail.com>
2023-08-31 13:10:15 +0700

    Ajarin Puh

---
+++ BetterCalculator.py
@@ -0,0 +1,18 @@
+import math
+
+while True:
+    print("KALKULATOR")
+    print("Kakulator sederhana yang dapat melakukan operasi penjumlahan, pengurangan, dan faktorial")
+    print("---------------------")
+    
+    operasi = input("Silahkan masukan operasi matematika yang ingin dilakukan: ")
+    hasil = 0
+    for i in operasi.split():
+        if(i == "+"):
+            pass
+        elif(i == "-"):
+            pass
+        elif(i.find('!')):
+            print(math.factorial(int(i.strip('!'))))
+        else:
+            hasil += int(i)
