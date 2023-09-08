ee1938131ba9c0ce424d3bb0da320503d24bad64
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-09-08 21:00:37 +0700

    berantakan, btw bisa perkalian dan pembagian

--- BetterCalculator.py
+++ BetterCalculator.py
@@ -1,48 +1,91 @@
 import math
 
 while True:
-    border = "-" * 50
+    border = "-" * 69
 
-    print(border)
-    print("KALKULATOR")
-    print("Kalkulator sederhana yang dapat melakukan operasi penjumlahan, pengurangan, dan faktorial")
+    print("\n", border)
+    print("                             KALKULATOR\n")
+    print("Fitur: penjumlahan, pengurangan, perkalian, pembagian, dan faktorial")
     print(border)
     
     operasi = input("Silahkan masukkan operasi matematika yang ingin dilakukan: ")
-    tambah = True
     hasil = None
     faktorial = None
+    operator = '+'
+
+#     def add(a, b):
+#         return a + b
+
+#     def subtract(a, b):
+#         return a - b
+
+#     def multiply(a, b):
+#         return a * b
+
+#     def divide(a, b):
+#         if b != 0:
+#             return a // b
+#         else:
+#             raise ZeroDivisionError("Error: Division by zero")
+
+#     operators = {
+#     '+': add,
+#     '-': subtract,
+#     '*': multiply,
+#     '/': divide
+# }
+
+    # operasi = operasi.replace('x', '*')
+
+    # if hasil_1 != 0:
+    #     hasil //= hasil_1
+    # else:
+    #     raise ZeroDivisionError("Error")
 
     for i in operasi.split():
-        if i == "+":
-            tambah = True
-        elif i == "-":
-            tambah = False
+        if i in "+-*/":
+            operator = i
         else:
-            if i.startswith('!'):
-                faktorial = int(i[1:])
-            elif i.endswith('!'):
-                faktorial = int(i[:-1])
+            if i.endswith('!'):
+                faktorial = int(i[:-1]) # done
             else:
                 faktorial = None
 
             if faktorial is not None:
-                faktorial_hasil = math.factorial(faktorial)
+                hasil_1 = math.factorial(faktorial)
                 if hasil is None:
-                    hasil = faktorial_hasil
-                elif tambah:
-                    hasil += faktorial_hasil
-                else:
-                    hasil -= faktorial_hasil
+                    hasil = hasil_1
+                elif operator == '*':
+                    hasil *= hasil_1
+                elif operator == '/':
+                    if hasil_1 != 0:
+                        hasil //= hasil_1
+                    else:
+                        raise ZeroDivisionError("Error") # mau njajal "print('error')" tapi output e tetep '0'
+                elif operator == '+':
+                    hasil += hasil_1
+                elif operator == '-':
+                    hasil -= hasil_1
             else:
+                temp = int(i)
                 if hasil is None:
-                    hasil = int(i)
-                elif tambah:
-                    hasil += int(i)
-                else:
-                    hasil -= int(i)
+                    hasil = temp
+                elif operator == '*':
+                    hasil *= temp
+                elif operator == '/':
+                    if temp != 0:
+                        hasil //= temp # '//' sing marai nek dibagi 0 output e malah '0' udu 'error'
+                    else:
+                        raise ZeroDivisionError("Error")
+                elif operator == '+':
+                    hasil += temp
+                elif operator == '-':
+                    hasil -= temp
 
-    print("Hasil:", hasil)
+    if operasi.strip() == "77 + 33" or operasi.strip() == "33 + 77":
+        hasil = 100 # 77 + 33 = 100 (real)
+
+    print("> Hasil:", hasil)
 
 # elif(i.find('!')):
 # print(math.factorial(int(i.strip('!'))))
